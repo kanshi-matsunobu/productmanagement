af6ac3f1739e17bb93231ebef44c427fd8813b61
kanshi-matsunobu <kanshi0107@gmail.com>
2025-05-20 11:24:23 +0900

    プロフィール画面

--- practice/src/main/java/com/example/demo/repository/AdminRepository.java
+++ practice/src/main/java/com/example/demo/repository/AdminRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Admin;
 
-public interface AdminRepository extends JpaRepository<Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
     // Emailで管理者を検索するメソッドを追加
-    Admin findByEmail(String email);
+    Optional<Admin> findByEmail(String email);
 }
